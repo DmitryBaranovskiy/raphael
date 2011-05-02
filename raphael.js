ff8be2949b36104eac2edee7f675e78f057224dc
Kelley Reynolds <kelley@insidesystems.net>
2011-05-02 11:00:45 -0400

    Text elements that are created with a blank string break in chrome and firefox

--- raphael.js
+++ raphael.js
@@ -2373,7 +2373,7 @@
                 var texts = Str(params.text).split("\n"),
                     tspans = [],
                     tspan;
-                for (var i = 0, ii = texts.length; i < ii; i++) if (texts[i]) {
+                for (var i = 0, ii = texts.length; i < ii; i++) if (typeof texts[i] == 'string') {
                     tspan = $("tspan");
                     i && $(tspan, {dy: fontSize * leading, x: a.x});
                     tspan.appendChild(g.doc.createTextNode(texts[i]));
