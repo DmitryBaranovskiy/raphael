3ccf8b7d39734d74229a62abbf0221ae83e817eb
Richard Zurad <rzurad@gmail.com>
2011-10-20 06:56:29 +0000

    Removing uneeded text rotation in tag tooltip

--- plugins/raphael.tooltips.js
+++ plugins/raphael.tooltips.js
@@ -16,9 +16,7 @@ Raphael.fn.tag = function (x, y, text, angle, r) {
             this[0].attr({path: ["M", x, y + r, "c", -R, 0, -r, R - r, -r, -r, 0, -R, r - R, -r, r, -r, R, 0, r, r - R, r, r, 0, R, R - r, r, -r, r, "M", x + dx, y - bb.height / 2 - d, "a", r + d, r + d, 0, 1, 0, 0, bb.height + 2 * d, "l", r + d - dx + bb.width + 2 * d, 0, 0, -bb.height - 2 * d, "L", x + dx, y - bb.height / 2 - d].join(",")});
         }
         this[1].attr({x: x + r + d + bb.width / 2, y: y});
-        angle = (360 - angle) % 360;
         this.rotate(angle, x, y);
-        angle > 90 && angle < 270 && this[1].attr({x: x - r - d - bb.width / 2, y: y, rotation: [180 + angle, x, y]});
         return this;
     };
     return res.update();
