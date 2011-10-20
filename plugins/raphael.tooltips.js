07ecae3704a70e2b73d90d4915ea318519ac4115
Richard Zurad <rzurad@gmail.com>
2011-10-20 06:04:58 +0000

    Adding 'tag' tooltip from old g.Raphael

--- plugins/raphael.tooltips.js
+++ plugins/raphael.tooltips.js
@@ -22,6 +22,38 @@ Raphael.fn.tag = function (x, y, text, angle, r) {
         angle > 90 && angle < 270 && this[1].attr({x: x - r - d - bb.width / 2, y: y, rotation: [180 + angle, x, y]});
         return this;
     };
-    res.update();
-    return res;
+    return res.update();
+};
+Raphael.fn.popup = function (x, y, text, dir, size) {
+    dir = dir == null ? 2 : dir > 3 ? 3 : dir;
+    size = size || 5;
+    text = text || "$9.99";
+    var res = this.set(),
+        mmax = Math.max,
+        d = 3;
+    res.push(this.path().attr({fill: "#000", stroke: "#000"}));
+    res.push(this.text(x, y, text).attr({fill: "#fff", font: "12px Arial, sans-serif", "font-family": "Helvetica, Arial"}));
+    res.update = function (X, Y, withAnimation) {
+        X = X || x;
+        Y = Y || y;
+        var bb = this[1].getBBox(),
+            w = bb.width / 2,
+            h = bb.height / 2,
+            dx = [0, w + size * 2, 0, -w - size * 2],
+            dy = [-h * 2 - size * 3, -h - size, 0, -h - size],
+            p = ["M", X - dx[dir], Y - dy[dir], "l", -size, (dir == 2) * -size, -mmax(w - size, 0), 0, "a", size, size, 0, 0, 1, -size, -size,
+                "l", 0, -mmax(h - size, 0), (dir == 3) * -size, -size, (dir == 3) * size, -size, 0, -mmax(h - size, 0), "a", size, size, 0, 0, 1, size, -size,
+                "l", mmax(w - size, 0), 0, size, !dir * -size, size, !dir * size, mmax(w - size, 0), 0, "a", size, size, 0, 0, 1, size, size,
+                "l", 0, mmax(h - size, 0), (dir == 1) * size, size, (dir == 1) * -size, size, 0, mmax(h - size, 0), "a", size, size, 0, 0, 1, -size, size,
+                "l", -mmax(w - size, 0), 0, "z"].join(","),
+            xy = [{x: X, y: Y + size * 2 + h}, {x: X - size * 2 - w, y: Y}, {x: X, y: Y - size * 2 - h}, {x: X + size * 2 + w, y: Y}][dir];
+        xy.path = p;
+        if (withAnimation) {
+            this.animate(xy, 500, ">");
+        } else {
+            this.attr(xy);
+        }
+        return this;
+    };
+    return res.update(x, y);
 };
