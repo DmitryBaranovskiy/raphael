3142e7566c32bbd7ac8b2c820ab1829d75c8e0eb
Richard Zurad <rzurad@gmail.com>
2011-10-22 21:24:00 +0000

    Moved 'lighter', 'darker', and 'original' Element prototype functions from graphael into a plugin called 'brightness'

---
+++ plugins/raphael.brightness.js
@@ -0,0 +1,40 @@
+Raphael.el.lighter = function (times) {
+    times = times || 2;
+
+    var fs = [this.attrs.fill, this.attrs.stroke];
+
+    this.fs = this.fs || [fs[0], fs[1]];
+
+    fs[0] = Raphael.rgb2hsb(Raphael.getRGB(fs[0]).hex);
+    fs[1] = Raphael.rgb2hsb(Raphael.getRGB(fs[1]).hex);
+    fs[0].b = Math.min(fs[0].b * times, 1);
+    fs[0].s = fs[0].s / times;
+    fs[1].b = Math.min(fs[1].b * times, 1);
+    fs[1].s = fs[1].s / times;
+
+    this.attr({fill: "hsb(" + [fs[0].h, fs[0].s, fs[0].b] + ")", stroke: "hsb(" + [fs[1].h, fs[1].s, fs[1].b] + ")"});
+};
+
+Raphael.el.darker = function (times) {
+    times = times || 2;
+
+    var fs = [this.attrs.fill, this.attrs.stroke];
+
+    this.fs = this.fs || [fs[0], fs[1]];
+
+    fs[0] = Raphael.rgb2hsb(Raphael.getRGB(fs[0]).hex);
+    fs[1] = Raphael.rgb2hsb(Raphael.getRGB(fs[1]).hex);
+    fs[0].s = Math.min(fs[0].s * times, 1);
+    fs[0].b = fs[0].b / times;
+    fs[1].s = Math.min(fs[1].s * times, 1);
+    fs[1].b = fs[1].b / times;
+
+    this.attr({fill: "hsb(" + [fs[0].h, fs[0].s, fs[0].b] + ")", stroke: "hsb(" + [fs[1].h, fs[1].s, fs[1].b] + ")"});
+};
+
+Raphael.el.original = function () {
+    if (this.fs) {
+        this.attr({ fill: this.fs[0], stroke: this.fs[1] });
+        delete this.fs;
+    }
+};
