af011e1ecf10372d81abfccc578ff3111e5285f4
Richard Zurad <rzurad@gmail.com>
2011-10-23 19:28:58 +0000

    Ported 'blob' to Element prototype. Added aliases on Set prototype for all tooltips. 'flag', 'tag', and 'drop' are currently broken on sets

--- plugins/raphael.tooltips.js
+++ plugins/raphael.tooltips.js
@@ -3,8 +3,11 @@
  */
 Raphael.el.popup = function (x, y, dir, size) {
     var dirs = { down: 0, left: 1, up: 2, right: 3 },
+        paper = this.paper || this[0].paper,
         mmax = Math.max,
         bb, w, h, dx, dy, p, xy;
+
+    if (!paper) return;
 
     dir = dir == null ? 2 : typeof dirs[dir] != 'undefined' ? dirs[dir] : 2;
     size = size || 5;
@@ -36,12 +39,16 @@ Raphael.el.popup = function (x, y, dir, size) {
     ][dir];
 
     this.translate(xy.x - w - bb.x, xy.y - h - bb.y);
-    return this.paper.path(p).attr({ fill: "#000", stroke: "none" }).insertBefore(this.node ? this : this[0]);
+    return paper.path(p).attr({ fill: "#000", stroke: "none" }).insertBefore(this.node ? this : this[0]);
 };
 
 Raphael.el.tag = function (x, y, angle, r) {
     var d = 3,
-        p = this.paper.path().attr({ fill: '#000', stroke: '#000' }),
+        paper = this.paper || this[0].paper;
+
+    if (!paper) return;
+
+    var p = paper.path().attr({ fill: '#000', stroke: '#000' }),
         bb = this.getBBox(),
         dx, R, center, tmp;
 
@@ -93,9 +100,45 @@ Raphael.el.tag = function (x, y, angle, r) {
     return p.insertBefore(this.node ? this : this[0]);
 };
 
+Raphael.el.drop = function (x, y, angle) {
+    var bb = this.getBBox(),
+        paper = this.paper || this[0].paper,
+        center, size, p;
+
+    if (!paper) return;
+
+    switch (this.type) {
+        case 'text':
+        case 'circle':
+        case 'ellipse': center = true; break;
+        default: center = false;
+    }
+
+    angle = angle || 0;
+
+    x = x || bb.x;
+    y = y || bb.y;
+    size = Math.max(bb.width, bb.height) + Math.min(bb.width, bb.height);
+    p = paper.path([
+        "M", x, y,
+        "l", size, 0,
+        "A", size * .4, size * .4, 0, 1, 0, x + size * .7, y - size * .7,
+        "z"
+    ]).attr({fill: "#000", stroke: "none"}).rotate(22.5 - angle, x, y);
+
+    angle = (angle + 90) * Math.PI / 180;
+    this.attr({ x: (x + size * Math.sin(angle)) - (center ? 0 : bb.width / 2), y: (y + size * Math.cos(angle)) - (center ? 0 : bb.height / 2) });
+
+    return p.insertBefore(this.node ? this : this[0]);
+};
+
 Raphael.el.flag = function (x, y, angle) {
     var d = 3,
-        p = this.paper.path().attr({ fill: '#000', stroke: '#000' }),
+        paper = this.paper || this[0].paper;
+
+    if (!paper) return;
+
+    var p = paper.path().attr({ fill: '#000', stroke: '#000' }),
         bb = this.getBBox(),
         h = bb.height / 2,
         center;
@@ -130,9 +173,63 @@ Raphael.el.flag = function (x, y, angle) {
 
 Raphael.el.label = function () {
     var bb = this.getBBox(),
+        paper = this.paper || this[0].paper,
         r = Math.min(20, bb.width + 10, bb.height + 10) / 2;
 
-    return this.paper.rect(bb.x - r / 2, bb.y - r / 2, bb.width + r, bb.height + r, r).attr({ stroke: 'none', fill: '#000' }).insertBefore(this.node ? this : this[0]);
+    if (!paper) return;
+
+    return paper.rect(bb.x - r / 2, bb.y - r / 2, bb.width + r, bb.height + r, r).attr({ stroke: 'none', fill: '#000' }).insertBefore(this.node ? this : this[0]);
+};
+
+Raphael.el.blob = function (x, y, angle) {
+    var bb = this.getBBox(),
+        rad = Math.PI / 180,
+        paper = this.paper || this[0].paper,
+        p, center, size;
+
+    if (!paper) return;
+
+    switch (this.type) {
+        case 'text':
+        case 'circle':
+        case 'ellipse': center = true; break;
+        default: center = false;
+    }
+
+    p = paper.path().attr({ fill: "#000", stroke: "none" });
+    angle = (+angle + 1 ? angle : 45) + 90;
+    size = Math.min(bb.height, bb.width);
+    x = x || (center ? bb.x + bb.width / 2 : bb.x);
+    y = y || (center ? bb.y + bb.height / 2 : bb.y);
+
+    var w = Math.max(bb.width + size, size * 25 / 12),
+        h = Math.max(bb.height + size, size * 25 / 12),
+        x2 = x + size * Math.sin((angle - 22.5) * rad),
+        y2 = y + size * Math.cos((angle - 22.5) * rad),
+        x1 = x + size * Math.sin((angle + 22.5) * rad),
+        y1 = y + size * Math.cos((angle + 22.5) * rad),
+        dx = (x1 - x2) / 2,
+        dy = (y1 - y2) / 2,
+        rx = w / 2,
+        ry = h / 2,
+        k = -Math.sqrt(Math.abs(rx * rx * ry * ry - rx * rx * dy * dy - ry * ry * dx * dx) / (rx * rx * dy * dy + ry * ry * dx * dx)),
+        cx = k * rx * dy / ry + (x1 + x2) / 2,
+        cy = k * -ry * dx / rx + (y1 + y2) / 2;
+
+    p.attr({
+        x: cx,
+        y: cy,
+        path: [
+            "M", x, y,
+            "L", x1, y1,
+            "A", rx, ry, 0, 1, 1, x2, y2,
+            "z"
+        ].join(",")
+    });
+
+    this.translate(cx - bb.x - bb.width / 2, cy - bb.y - bb.height / 2);
+
+    return p.insertBefore(this.node ? this : this[0]);
 };
 
 /*
@@ -170,6 +267,7 @@ Raphael.fn.flag = function (x, y, text, angle) {
     return set.push(text.flag(x, y, angle), text);
 };
 
+//drop alias on the paper instance to create an easy text drop
 Raphael.fn.drop = function (x, y, text, angle) {
     var set = this.set();
 
@@ -177,68 +275,22 @@ Raphael.fn.drop = function (x, y, text, angle) {
     return set.push(text.drop(x, y, angle), text);
 };
 
-//drop tooltip. can only be done with text at the moment, just like old graphel
-Raphael.el.drop = function (x, y, angle) {
-    var bb = this.getBBox(),
-        center, size, p;
+//blob alias on the paper instance to create an easy text blob
+Raphael.fn.blob = function (x, y, text, angle) {
+    var set = this.set();
 
-    switch (this.type) {
-        case 'text':
-        case 'circle':
-        case 'ellipse': center = true; break;
-        default: center = false;
-    }
-
-    angle = angle || 0;
-
-    x = x || bb.x;
-    y = y || bb.y;
-    size = Math.max(bb.width, bb.height) + Math.min(bb.width, bb.height),
-    p = this.paper.path([
-        "M", x, y,
-        "l", size, 0,
-        "A", size * .4, size * .4, 0, 1, 0, x + size * .7, y - size * .7,
-        "z"
-    ]).attr({fill: "#000", stroke: "none"}).rotate(22.5 - angle, x, y);
-
-    angle = (angle + 90) * Math.PI / 180;
-    this.attr({ x: (x + size * Math.sin(angle)) - (center ? 0 : bb.width / 2), y: (y + size * Math.cos(angle)) - (center ? 0 : bb.height / 2) });
-
-    return p.insertBefore(this.node ? this : this[0]);
+    text = this.text(x, y, text).attr({ fill: '#fff', font: '12px Arial, sans-serif', 'font-family': 'Helvetica, Arial' });
+    return set.push(text.blob(x, y, angle), text);
 };
 
-//blob tooltip. can only be done with text at the moment, just like old graphael
-Raphael.fn.blob = function (x, y, text, size, angle) {
-    angle = (+angle + 1 ? angle : 45) + 90;
-    size = size || 12;
-    var rad = Math.PI / 180,
-        fontSize = size * 12 / 12;
-    var res = this.set();
-    res.push(this.path().attr({fill: "#000", stroke: "none"}));
-    res.push(this.text(x + size * Math.sin((angle) * rad), y + size * Math.cos((angle) * rad) - fontSize / 2, text).attr({font: "12px Arial, sans-serif", "font-size": fontSize, fill: "#fff"}));
-    res.update = function (X, Y, withAnimation) {
-        X = X || x;
-        Y = Y || y;
-        var bb = this[1].getBBox(),
-            w = Math.max(bb.width + fontSize, size * 25 / 12),
-            h = Math.max(bb.height + fontSize, size * 25 / 12),
-            x2 = X + size * Math.sin((angle - 22.5) * rad),
-            y2 = Y + size * Math.cos((angle - 22.5) * rad),
-            x1 = X + size * Math.sin((angle + 22.5) * rad),
-            y1 = Y + size * Math.cos((angle + 22.5) * rad),
-            dx = (x1 - x2) / 2,
-            dy = (y1 - y2) / 2,
-            rx = w / 2,
-            ry = h / 2,
-            k = -Math.sqrt(Math.abs(rx * rx * ry * ry - rx * rx * dy * dy - ry * ry * dx * dx) / (rx * rx * dy * dy + ry * ry * dx * dx)),
-            cx = k * rx * dy / ry + (x1 + x2) / 2,
-            cy = k * -ry * dx / rx + (y1 + y2) / 2;
-        if (withAnimation) {
-            this.animate({x: cx, y: cy, path: ["M", x, y, "L", x1, y1, "A", rx, ry, 0, 1, 1, x2, y2, "z"].join(",")}, 500, ">");
-        } else {
-            this.attr({x: cx, y: cy, path: ["M", x, y, "L", x1, y1, "A", rx, ry, 0, 1, 1, x2, y2, "z"].join(",")});
-        }
-        return this;
-    };
-    return res.update(x, y);
-};
+
+//put alias on the set prototype
+(function () {
+    var funcs = ['popup', 'tag', 'flag', 'label', 'drop', 'blob'];
+
+    for (var f in funcs) (function (name) {
+        Raphael.st[name] = function () {
+            Raphael.el[name].apply(this, arguments);
+        };
+    })(funcs[f]);
+})();
