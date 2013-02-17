2929bfdf6c016def3a9c2ba4c5b875219e428d02
Tomas Alabes <tomas.alabes@gmail.com>
2013-02-17 19:41:36 -0300

    Corrected typos of documentation

--- raphael.svg.js
+++ raphael.svg.js
@@ -865,7 +865,7 @@ window.Raphael && window.Raphael.svg && function (R) {
      * Element.remove
      [ method ]
      **
-     * Removes element form the paper.
+     * Removes element from the paper.
     \*/
     elproto.remove = function () {
         if (this.removed || !this.node.parentNode) {
@@ -924,7 +924,7 @@ window.Raphael && window.Raphael.svg && function (R) {
      = (object) object of attributes if nothing is passed in.
      > Possible parameters
      # <p>Please refer to the <a href="http://www.w3.org/TR/SVG/" title="The W3C Recommendation for the SVG language describes these properties in detail.">SVG specification</a> for an explanation of these parameters.</p>
-     o arrow-end (string) arrowhead on the end of the path. The format for string is `<type>[-<width>[-<length>]]`. Possible types: `classic`, `block`, `open`, `oval`, `diamond`, `none`, width: `wide`, `narrow`, `midium`, length: `long`, `short`, `midium`.
+     o arrow-end (string) arrowhead on the end of the path. The format for string is `<type>[-<width>[-<length>]]`. Possible types: `classic`, `block`, `open`, `oval`, `diamond`, `none`, width: `wide`, `narrow`, `medium`, length: `long`, `short`, `midium`.
      o clip-rect (string) comma or space separated values: x, y, width and height
      o cursor (string) CSS type of the cursor
      o cx (number) the x-axis coordinate of the center of the circle, or ellipse
