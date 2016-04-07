db2b2f2b91d898015f1993f0039dd9280a95d264
Tomas Alabes <tomas.alabes@oracle.com>
2016-04-06 21:36:44 -0700

    webpack ready, just eslint missing

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,48 @@
-var webpack = require("webpack");
+"use strict";
+
+const webpack = require("webpack");
+const fs = require("fs");
+
+const args = process.argv;
+
+let plugins = [
+	new webpack.BannerPlugin(fs.readFileSync('./dev/banner.txt', 'utf8'),{ raw: true, entryOnly: true })
+];
+let externals = [];
+let filename = "raphael";
+
+
+if(args.indexOf('--no-deps') !== -1){
+	console.log('Building version without deps');
+	externals.push("eve");
+	filename += ".no-deps"
+}
+
+if(args.indexOf('--min') !== -1){
+	console.log('Building minified version');
+	plugins.push(
+		new webpack.optimize.UglifyJsPlugin({
+			compress:{
+				dead_code: false,
+				unused: false
+			}
+		})
+	);
+	filename += ".min"
+}
 
 module.exports = {
 	entry: './dev/raphael.amd.js',
 	output: {
-		filename: "raphael-wp.js",
+		filename: filename + ".js",
 		libraryTarget: "umd",
 		library: "Raphael",
 		umdNamedDefine: true
 	},
 
-	target: "web",
+	externals: externals,
 
-	externals: [
-	],
-
-	loaders: [
-		
-	],
-	plugins: [
-		
-	],
-
-	resolveLoader: {
-		
-	},
+	plugins: plugins,
 
 	resolve: {
 		modulesDirectories: ["bower_components"]
