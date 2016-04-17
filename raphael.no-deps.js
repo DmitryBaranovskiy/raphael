7b6c9ab4800205a7281196710057cb52fdacd1ce
Feram <feram@adriansieber.com>
2016-04-17 21:25:22 +0000

    Fix typos in raphael.no-deps.js

--- raphael.no-deps.js
+++ raphael.no-deps.js
@@ -2913,7 +2913,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	     * Raphael.el
 	     [ property (object) ]
 	     **
-	     * You can add your own method to elements. This is usefull when you want to hack default functionality or
+	     * You can add your own method to elements. This is useful when you want to hack default functionality or
 	     * want to wrap some common transformation or attributes in one method. In difference to canvas methods,
 	     * you can redefine element method at any time. Expending element methods wouldn’t affect set.
 	     > Usage
@@ -3160,7 +3160,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	     * Element.data
 	     [ method ]
 	     **
-	     * Adds or retrieves given value asociated with given key.
+	     * Adds or retrieves given value associated with given key.
 	     **
 	     * See also @Element.removeData
 	     > Parameters
@@ -3269,7 +3269,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	     - mcontext (object) #optional context for moving handler
 	     - scontext (object) #optional context for drag start handler
 	     - econtext (object) #optional context for drag end handler
-	     * Additionaly following `drag` events will be triggered: `drag.start.<id>` on start,
+	     * Additionally following `drag` events will be triggered: `drag.start.<id>` on start,
 	     * `drag.end.<id>` on end and `drag.move.<id>` on every move. When element will be dragged over another element
 	     * `drag.over.<id>` will be fired as well.
 	     *
@@ -5381,7 +5381,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        return token || E;
 	    };
 	    /*\
-	     * Raphael.fullfill
+	     * Raphael.fulfill
 	     [ method ]
 	     **
 	     * A little bit more advanced format function than @Raphael.format. Replaces construction of type “`{<name>}`” to the corresponding argument.
@@ -5393,7 +5393,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	     = (string) formated string
 	     > Usage
 	     | // this will draw a rectangular shape equivalent to "M10,20h40v50h-40z"
-	     | paper.path(Raphael.fullfill("M{x},{y}h{dim.width}v{dim.height}h{dim['negative width']}z", {
+	     | paper.path(Raphael.fulfill("M{x},{y}h{dim.width}v{dim.height}h{dim['negative width']}z", {
 	     |     x: 10,
 	     |     y: 20,
 	     |     dim: {
@@ -5403,7 +5403,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	     |     }
 	     | }));
 	    \*/
-	    R.fullfill = (function () {
+	    R.fulfill = (function () {
 	        var tokenRegex = /\{([^\}]+)\}/g,
 	            objNotationRegex = /(?:(?:^|\.)(.+?)(?=\[|\.|$|\()|\[('|")(.+?)\2\])(\(\))?/g, // matches .xxxxx or ["xxxxx"] to run over object properties
 	            replacer = function (all, key, obj) {
@@ -6865,7 +6865,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	     * Paper.renderfix
 	     [ method ]
 	     **
-	     * Fixes the issue of Firefox and IE9 regarding subpixel rendering. If paper is dependant
+	     * Fixes the issue of Firefox and IE9 regarding subpixel rendering. If paper is dependent
 	     * on other elements after reflow it could shift half pixel which cause for lines to lost their crispness.
 	     * This method fixes the issue.
 	     **
