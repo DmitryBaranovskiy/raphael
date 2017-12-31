7ace67e5be32387a06ead1e32eeb6c3e80435973
Jishnu Mohan <jishnu7@gmail.com>
2017-12-31 13:39:59 +0530

    upgrade webpack to v3.x
    known issue: es-lint validation is broken.

--- webpack.config.js
+++ webpack.config.js
@@ -2,63 +2,68 @@
 
 const webpack = require("webpack");
 const fs = require("fs");
+const path = require("path");
 
-const args = process.argv;
-
-let plugins = [
-	new webpack.BannerPlugin(fs.readFileSync('./dev/banner.txt', 'utf8'), { raw: true, entryOnly: true })
-];
-let externals = [];
-let filename = "raphael";
-
-
-if(args.indexOf('--no-deps') !== -1){
-	console.log('Building version without deps');
-	externals.push("eve");
-	filename += ".no-deps"
-}
-
-if(args.indexOf('--min') !== -1){
-	console.log('Building minified version');
-	plugins.push(
-		new webpack.optimize.UglifyJsPlugin({
-			compress:{
-				dead_code: false,
-				unused: false
-			}
+module.exports = function(env) {
+	let plugins = [
+		new webpack.BannerPlugin({
+			banner: fs.readFileSync('./dev/banner.txt', 'utf8'),
+			raw: true,
+			entryOnly: true
 		})
-	);
-	filename += ".min"
-}
+	];
+	let externals = [];
+	let filename = "raphael";
 
-module.exports = {
-	entry: './dev/raphael.amd.js',
-	output: {
-		filename: filename + ".js",
-		libraryTarget: "umd",
-		library: "Raphael"
-	},
-
-	externals: externals,
-
-	plugins: plugins,
-
-	loaders: [
-  		{
-  			test: /\.js$/, 
-  			loader: "eslint-loader", 
-  			include: "./dev/"
-  		}
-	],
-  	
-	eslint: {
-    	configFile: './.eslintrc'
-  	},
-
-	resolve: {
-		modulesDirectories: ["bower_components"],
-		alias: {
-			"eve": "eve-raphael/eve"
-		}
+	if(env && env.noDeps){
+		console.log('Building version without deps');
+		externals.push("eve");
+		filename += ".no-deps"
 	}
+
+	if(env && env.min){
+		console.log('Building minified version');
+		plugins.push(
+			new webpack.optimize.UglifyJsPlugin({
+				compress:{
+					dead_code: false,
+					unused: false
+				}
+			})
+		);
+		filename += ".min"
+	}
+
+	return {
+		entry: './dev/raphael.amd.js',
+		output: {
+			filename: filename + ".js",
+			libraryTarget: "umd",
+			library: "Raphael"
+		},
+
+		externals: externals,
+
+		plugins: plugins,
+
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					loader: "eslint-loader",
+					include: path.join(__dirname, "dev"),
+					options: {
+						configFile: path.join(__dirname, '.eslintrc.js')
+					}
+				}
+			],
+		},
+
+		resolve: {
+			modules: ["bower_components", "node_modules"],
+			alias: {
+				"eve": "eve-raphael/eve"
+			}
+		}
+	};
 };
