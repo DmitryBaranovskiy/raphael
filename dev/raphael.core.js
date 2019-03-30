bf3dcd35317f76f915bcd04ed9db36a1b3775c4d
Tomas Alabes <tomasAlabes@users.noreply.github.com>
2019-03-30 08:08:06 -0700

    fix: Updated version string in raphael.core
    Reported in #1095

--- dev/raphael.core.js
+++ dev/raphael.core.js
@@ -67,7 +67,7 @@ define(["eve"], function(eve) {
             }
         }
     }
-    R.version = "2.2.0";
+    R.version = "2.2.8";
     R.eve = eve;
     var loaded,
         separator = /[, ]+/,
